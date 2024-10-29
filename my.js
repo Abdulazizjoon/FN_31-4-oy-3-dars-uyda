b63d07f649b37d9d96c495542587026d3151e3c1
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-10-29 19:03:27 +0500

    .

---
+++ my.js
@@ -0,0 +1,114 @@
+//1-misol
+// let a=5
+// if (a>0) {
+//     console.log('a soni musbet');
+// }
+
+//2-misol
+// let a = +prompt('son kiriting')
+// if (a%2==1) {
+//     console.log(`${a} soni toq`);
+// } else {
+//     console.log(`${a} soni juft`);
+// }
+
+//3-misol
+// let a = +prompt('son kiriting')
+// if (a%2==0) {
+//     console.log(`${a} soni juft`);
+// } else {
+//     console.log(`${a} soni toq`);
+// }
+
+//4-misol
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// if (a > 2 && b < 3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//5-misol
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// if (a >= 2 || b < -2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//6-misol
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+// let c = +prompt("son kiriting");
+
+// if (a <= b <= c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//7-misol
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+// let c = +prompt("son kiriting");
+
+// if (a < b < c || c < b < a) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//8-misol
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// if (a % 2 == 1 && b % 2 == 1) {
+//   console.log(`${a} va ${b} ssonlari toq son`);
+// } else {
+//   console.log(`${a} va ${b} ssonlari toq son emas`);
+// }
+
+//9-misol
+// let a = +prompt("son kiriting");
+// let b = +prompt("son kiriting");
+
+// if (a % 2 == 1 || b % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//10-misol
+// let a = 5;
+// let b = 6;
+
+// if ((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//11-misol
+// let a = 5;
+// let b = 6;
+
+// if ((a % 2 == 1 && b & (2 == 1)) || (a % 2 == 0 && b % 2 == 0)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//12-misol
+let a = 5;
+let b = 6;
+
+if ((a % 2 == 1 && b & (2 == 1)) || (a % 2 == 0 && b % 2 == 0)) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
